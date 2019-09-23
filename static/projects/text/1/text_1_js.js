f805218003c96c471b95acd159482dd8b0907f47
Vincent Giles <13183572+vgiles@users.noreply.github.com>
2019-09-23 22:20:12 +1000

    After local testing, removed excess logic that seemed to be breaking the borders.

--- static/projects/text/1/text_1_js.js
+++ static/projects/text/1/text_1_js.js
@@ -28,22 +28,21 @@ function renderText() {
     textSize(random(40));
     var random_times = noise(500);
     var margin = int(textWidth(words[i]));
-    console.log(words[i], margin);
     var wPoint = int(random_times*random(width));
     var hPoint = int(random_times*random(height));
     // this logic works well, but does not stop the bleeding from the edges
     if (wPoint >= (0 + margin) && wPoint <= (width - margin)) {
       fill(random(255) - 100);
-      text(words[i], wPoint + 20, hPoint + 40);
+      text(words[i], wPoint + 20, hPoint + 10);
+      console.log(words[i], wPoint, margin);
     } else if ((wPoint + margin) >= (width - margin)) {
       fill(random(255) - 100);
-      text(words[i], (wPoint - margin) - 40, hPoint + 40);
-    } else if (wPoint <= (0 + margin)) {
-      fill(random(255) - 100);
-      text(words[i], 50 + (wPoint + margin) + 40, hPoint + 40);
+      text(words[i], (wPoint + margin) - margin - 10, hPoint + 10);
+      console.log(words[i], wPoint, margin);
     } else {
       fill(random(255) - 100);
-      text(words[i], wPoint + margin, hPoint + 40);
+      text(words[i], margin, hPoint + 10);
+      console.log(words[i], wPoint, margin);
     }
   }
 }
