05c0fc6de9dc80b5b764655ebb7b6c851d8765c1
Vincent Giles <13183572+vgiles@users.noreply.github.com>
2019-09-23 19:56:44 +1000

    Conditional tests for placement.

--- static/projects/text/1/text_1_js.js
+++ static/projects/text/1/text_1_js.js
@@ -29,11 +29,11 @@ function renderText() {
     var margin = textWidth(words[i]);
     var wPoint = int(random_times*random(width));
     var hPoint = int(random_times*random(height));
-    if (wPoint >= (0 + margin)) {
+    if (wPoint >= (0 + margin) && wPoint <= (width - margin)) {
       fill(random(255) - 100);
       textSize(random(40));
       text(words[i], wPoint, hPoint);
-    } else if (wPoint >= width - margin ){
+    } else if (wPoint > (width - margin)){
       fill(random(255) - 100);
       textSize(random(40));
       text(words[i], wPoint - margin - 10, hPoint + 40);
