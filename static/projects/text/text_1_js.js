9580cb939f7ee4aaf39a01ffa2fbcb26507d6fd5
Vincent Giles <13183572+vgiles@users.noreply.github.com>
2019-09-23 16:25:43 +1000

    Moving all files to a different dir. Adjusting print objects.

--- static/projects/text/text_1_js.js
+++ static/projects/text/text_1_js.js
@@ -1,5 +1,5 @@
 // This code is taken from Allison Parrish with minor modifications by Vincent Giles
-// Thanks, Allison. https://creative-coding.decontextualize.com/text-and-type/ 
+// Thanks, Allison. https://creative-coding.decontextualize.com/text-and-type/
 var sourceText;
 var words;
 var loadedFile;
@@ -18,11 +18,8 @@ function draw() {
   background(255);
   textAlign(CENTER, CENTER);
   var n = int(random(loadedFile.length - 1));
-  print(n);
   var sourceText = loadedFile[n];
-  print(sourceText);
   var words = sourceText.split(" ");
-  print(words);
   for (var i = 0; i < words.length; i++) {
     var random_times = noise(500);
     var margin = textWidth(words[i]);
@@ -32,7 +29,7 @@ function draw() {
       fill(random(255) - 100);
       textSize(random(40));
       text(words[i], wPoint, hPoint);
-    } else { 
+    } else {
       fill(random(255) - 100);
       textSize(random(40));
       text(words[i], wPoint + margin, hPoint);
