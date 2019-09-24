986578ebc622f8f7d13ddc8a315284a98fec6c55
Vincent Giles <13183572+vgiles@users.noreply.github.com>
2019-09-24 10:45:43 +1000

    Initial commit from initial file setup. Establishing dev branch.

---
+++ static/projects/text/2/text_2.js
@@ -0,0 +1,11 @@
+// A project to create a generative 'score' for vocal performance from text.
+// Vincent Giles, 2019
+// Code borrowed from Allison Parish https://creative-coding.decontextualize.com/text-and-type/
+
+function setup() {
+  noLoop();
+}
+
+function draw() {
+
+}
