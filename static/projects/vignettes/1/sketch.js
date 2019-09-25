6f155bd84d40ddc706a8ba17e2499fa6d57ba908
Vincent Giles <13183572+vgiles@users.noreply.github.com>
2019-09-25 12:22:00 +1000

    Tidied up vignette code and removed unnecessary lines and variables.

--- static/projects/vignettes/1/sketch.js
+++ static/projects/vignettes/1/sketch.js
@@ -1,9 +1,9 @@
 /* Vignette #1 for any instrument.
-Vincent Giles, 2018
-*/
+Vincent Giles, 2018*/
+
 let x = 40;
-let y = 60;
 let xStart = 40;
+let y = 60;
 let yStart = 60;
 function basic_staff() {
   stroke(0);
@@ -15,7 +15,6 @@ function basic_staff() {
   }
   noStroke();
 }
-
 function initialise() {
   x = 40;
   y = 60;
@@ -24,7 +23,6 @@ function initialise() {
   composer_display();
   frameRate(5);
 }
-
 function composer_display() {
   let composerInfo = ["Vincent Giles, 2018", "Small Vignette #1", "Instructions: Use any instrument, assume any clef; improvise gesturally/spatially by the indicated animation."]
   fill(0);
@@ -35,39 +33,29 @@ function composer_display() {
   textSize(12);
   text(composerInfo[2], width/3, 12);
 }
-
 function setup() {
   createCanvas(1000, 200);
   background(255);
   initialise();
 }
-
 function draw() {
   newMusic();
 }
-
 function newMusic() {
   let r = random(200, 255);
   let g = random(100, 255);
   let b = random(150, 200);
   let a = random(100, 200);
   let diam = random(5, 30);
-  // let staff = basic_staff();
-  let staffXStart = 40;
-  let staffYStart = 60;
   let xRand = random(5, 50);
   let yRand = random(5, 40);
   let yChange = int(random(-1, 1));
-  //let xChange = int(random(-1, 1));
-  let timing = int(random(1, 15));
-  let xInc = int(random(1, 30));
-  //for (var p = 0; p < 960; p++) {
   if (x < 960) {
     x = x + xRand;
-    if (y <= staffYStart) {
-      y = staffYStart + yRand;
+    if (y <= 60) {
+      y = 60 + yRand;
     } else {
-      y = staffYStart + yRand*yChange;
+      y = 60 + (yRand*yChange);
     }
     fill(r, g, b, a);
     ellipse(x, y, diam, diam);
