ac54192a83b42b450f725148470606bcfdca6d44
Vincent Giles <13183572+vgiles@users.noreply.github.com>
2021-04-27 19:39:07 +1000

    Finished version 1

--- static/projects/concrete/concrete.js
+++ static/projects/concrete/concrete.js
@@ -1,5 +1,6 @@
 // define all the sounds for individual loading
 
+let border = 30;
 
 
 
@@ -45,7 +46,8 @@ function setup() {
       let w = random(200);
       let h = random(200);
       let r = random(10, 50);
-      let bop = new Fun(x, y, w, h);
+      let s = soundArray[i];
+      let bop = new Bork(x, y, w, h, s);
       rectangle.push(bop);
   }
 //   i = 0;
@@ -61,6 +63,7 @@ function draw() {
     for (i = 0; i < rectangle.length; i++) {
         // rectangle[i].move();
         rectangle[i].display();
+        rectangle[i].move();
     }
     // noLoop();
 }
@@ -70,7 +73,63 @@ function mousePressed() {
         getAudioContext().resume();
       }
     for (i = 0; i < rectangle.length; i++) {
-        rectangle[i].clicked(mouseX, mouseY);
+        rectangle[i].clicked(mouseX, mouseY, mouseY);
     }
     
+}
+
+
+
+// Constructors and such
+
+class Bork {
+    constructor(x, y, w, h, s) {
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height= h;
+        this.alpha = random(255);
+        this.randR = random(255);
+        this.randG = random(255);
+        this.randB = random(255);
+        this.sound = s;
+        // this.r = r;
+        // this.sound = soundArray[random(soundArray.length)];
+    }
+    
+    display() {
+        rectMode(CENTER);
+        // var randR = 220;
+        // var randG = 200);
+        // var randB = Math.floor(Math.random() * 255);
+        // var randA = Math.floor(Math.random() * 255);
+        var randRad = Math.floor(Math.random() * 255);
+        // fill(randR, randG, randB, this.alpha);
+        fill(this.randR, this.randG, this.randB, this.alpha);
+        rect(this.x, this.y, this.width, this.height);
+        // ellipse(this.x, this.y, this.r * 2);
+    }
+
+    move() {
+        this.x = this.x + random(-5, 5);
+        this.y = this.y + random(-5, 5);
+      }
+
+    clicked(px, py, sp) {
+        let d = dist(px, py, this.x, this.y);
+        let playbackRate = map(mouseY, 0.1, height, 2, 0);
+        playbackRate = constrain(playbackRate, 0.01, 4);
+        // let d2 = dist(this.x, this.y, this.width * 2, this.height * 2);
+        // console.log(d);
+        // console.log(d2);
+        if (d < this.width && d < this.height) {
+            this.alpha = random(255);
+            this.randR = random(255);
+            this.randG = random(255);
+            this.randB = random(255);
+            this.sound.play();
+            this.sound.rate(playbackRate);
+        }
+        // this.sound.play();
+    }
 }
