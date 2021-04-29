d24dd28993ea27959f5e08c03f18b70ee8626877
Vincent Giles <13183572+vgiles@users.noreply.github.com>
2021-04-29 19:13:40 +1000

    Doing initial setup for blorb

---
+++ static/projects/funtest/blorbs.js
@@ -0,0 +1,16 @@
+let sound0;
+function preload() {
+    soundFormats('mp3');
+    sound0 = loadSound('assets/audio/1');
+}
+function setup() {
+    createCanvas(720, 400);
+
+}
+
+
+// Constructor
+
+class Blorb {
+    constructor()
+}
