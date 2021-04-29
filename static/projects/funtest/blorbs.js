40fc85bf131648692a0a230861e5ddc94ef20944
Vincent Giles <13183572+vgiles@users.noreply.github.com>
2021-04-29 19:58:17 +1000

    Update blorbs.js
    Works with 50 blorbs. No audio yet.

--- static/projects/funtest/blorbs.js
+++ static/projects/funtest/blorbs.js
@@ -1,35 +1,66 @@
 let sound0;
 let blorb;
+let value;
+let blorbs = [];
 function preload() {
     soundFormats('mp3');
-    sound0 = loadSound('audio/000_Bottle');
+    sound0 = loadSound('audio/000_Bottle.mp3');
 }
+
+
 function setup() {
     createCanvas(720, 400);
-    let x = random(width);
-    let y = random(height);
-    let r = random(5, 15);
-    blorb = new Blorb(x, y, r);
+    frameRate(25);
+    for (i = 0; i < 50; i++) {
+        let x = random(width);
+        let y = random(height);
+        let r = random(5, 15);
+        let s = sound0;
+        blorb = new Blorb(x, y, r, s);
+        blorbs.push(blorb);
+    }
 }
 
 function draw() {
     background(220);
-    blorb.display();
+    for (i = 0; i < blorbs.length; i++)
+    {
+        blorbs[i].display();
+    }
 }
 
-
+function mouseMoved() {
+    for (i = 0; i < blorbs.length; i++) {
+        blorbs[i].hover(mouseX, mouseY);
+    }
+    
+}
 // Constructor
 
 class Blorb {
-    constructor(x, y, r) {
+    constructor(x, y, r, s) {
         this.x = x;
         this.y = y;
         this.r = r;
+        this.f = random(255);
+        this.s = s;
     }
 
     display() {
-        stroke(0);
-        fill(150);
+        stroke(20);
+        fill(this.f);
         ellipse(this.x, this.y, this.r * 2);
+    }
+
+    hover(px, py) {
+        let d = dist(px, py, this.x, this.y);
+        if (d < this.r)
+        {
+            this.f = random(255);
+            this.r = this.r + random(-2, 2);
+            this.x = this.x + random(-50, 50);
+            this.y = this.y + random(-50, 50);
+            this.s.play();
+        }
     }
 }
