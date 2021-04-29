89b045b2facdb0ceab510be62551e59ce1aba200
Vincent Giles <13183572+vgiles@users.noreply.github.com>
2021-04-29 19:27:14 +1000

    Update blorbs.js
    Drawing construction and loading soundfile complete

--- static/projects/funtest/blorbs.js
+++ static/projects/funtest/blorbs.js
@@ -1,16 +1,35 @@
 let sound0;
+let blorb;
 function preload() {
     soundFormats('mp3');
-    sound0 = loadSound('assets/audio/1');
+    sound0 = loadSound('audio/000_Bottle');
 }
 function setup() {
     createCanvas(720, 400);
+    let x = random(width);
+    let y = random(height);
+    let r = random(5, 15);
+    blorb = new Blorb(x, y, r);
+}
 
+function draw() {
+    background(220);
+    blorb.display();
 }
 
 
 // Constructor
 
 class Blorb {
-    constructor()
+    constructor(x, y, r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    display() {
+        stroke(0);
+        fill(150);
+        ellipse(this.x, this.y, this.r * 2);
+    }
 }
